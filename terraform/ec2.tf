b4a2b010b0d26057919df6b52985dc68662dd89b
rushidholecom <dholerushi.com@gmail.com>
2026-02-23 21:17:52 +0530

    create tf file for ec2

---
+++ terraform/ec2.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+
+    region = ap-southeast-1
+  
+}
+
+resource "aws_instance" "ec2-docker" {
+  
+  ami = ami-08d59269edddde222
+  instance_type = "t3micro"
+  key_name = "rr-rushi-singapore"
+  root_block_device {
+    volume_size = 20
+  }
+  vpc_security_group_ids = ["sg-0afd97e579140952f"]
+  tags = {
+    Name = "terraform-prac"
+  }
+}
